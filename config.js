b4c75f5a06d6016fb4a01f3cb579073d2b8b6de9
shakthitha <93185970+shakthitha@users.noreply.github.com>
2024-11-08 18:58:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
-    BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
+    SESSION_ID: process.env.SESSION_ID || 'C3xAzbiB#s9GaDGwyGhAB83kC53L4WCg7s4UeVXXWxkCxUAGmR8k', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://tharux:tharux@cluster0.xxrdh.mongodb.net/',    // Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94743660613',    // Enter Your Owner Number
+    BotNumber: process.env.BotNumber || '94743660613'    // Enter Your Bot Number
 };
